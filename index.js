14a4abd41223f924132d74e396a1d68d7244aee1
Darya Naji <daaryaa.naji@gmail.com>
2022-12-07 13:24:19 +0300

    completed assignmnet

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+
+const multiply = num1 * num2;
+
+// const random = 4;
+// Number.isInteger(random);
+
+const r1 = Math.floor((Math.random() *10) + 1);
+const r2 = Math.floor((Math.random() * 10) + 1);
+const random = r1 + r2;
+
+const num3 = 34
+const num4 = 10;
+const mod = num3 % num4;
+
+const max = Math.max(4, 15, 17, 20, 3, 18);
